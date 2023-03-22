948aa37bf660bdf30882698098ebc82ba82ff443
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-03-22 12:37:28 +0100

    Testowe podłączenie na poligonie 3

---
+++ js/test/test3.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+
+let scene, camera, renderer;
+
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth,
+  window.innerHeight,
+  0.0001,
+  10000
+);
+camera.position.set(0,0,20);
+renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const handleResize = () => {
+  const { innerWidth, innerHeight } = window;
+  renderer.setSize(innerWidth, innerHeight);
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+};
+
+window.addEventListener("resize", handleResize);
+
+const loop = () => {
+    renderer(scene, camera);
+    requestAnimationFrame(loop);
+}
