1df1b5346ba625392b77cd6146c5632765285d79
Michał Blachnicki <mrgregorybrzeczyszczykiewicz@proton.me>
2023-03-23 22:01:30 +0100

    Trening na poligonie 3. Utworzenie węzła ze skromną animacją.

--- js/test/test3.js
+++ js/test/test3.js
@@ -4,17 +4,32 @@ let scene, camera, renderer;
 
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(
-  40,
-  window.innerWidth,
-  window.innerHeight,
+  50,
+  window.innerWidth / window.innerHeight,
   0.0001,
   10000
 );
-camera.position.set(0, 0, 20);
+camera.position.set(0, 0, 40);
 renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
+
+const createTorus = (c=0xffffff, r=4.5,t=1,tSeg=180,rSeg=20,p=3,q=7) => {
+  const torusGeo = new THREE.TorusKnotGeometry(r, t, tSeg, rSeg, p, q);
+  const torusMat = new THREE.MeshBasicMaterial({ color: c});
+  return new THREE.Mesh(torusGeo, torusMat);
+};
+
+const createPointLight = (color=0xffffff, i=0.4) => {
+  return new THREE.PointLight(color, i);
+};
+
+const fTorus = createTorus();
+scene.add(fTorus);
+
+const l1 = createPointLight();
+l1.position.set(20,20,60);
+scene.add(l1);
 
 const handleResize = () => {
   const { innerWidth, innerHeight } = window;
@@ -25,6 +40,7 @@ const handleResize = () => {
 window.addEventListener("resize", handleResize);
 
 const loop = () => {
+  fTorus.rotation.y += 0.001;
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
 }
