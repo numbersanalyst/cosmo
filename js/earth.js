b36284c6425bf91a3599f1d132373599c5f4118e
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-03-27 20:51:39 +0200

    Dodanie bazowego kodu do podstrony earth.js

--- js/earth.js
+++ js/earth.js
@@ -1,4 +1,24 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const camera = new THREE.Camera();
-console.log(camera);
+let scene, camera, renderer;
+
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 0, 50);
+renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const loop = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(loop);
+}
+loop();
+
+const handleResize = () => {
+    const { innerWidth, innerHeight } = window;
+    renderer.setSize(innerWidth, innerHeight);
+    camera.aspect = innerWidth / innerHeight;
+    camera.updateProjectionMatrix();
+}
+window.addEventListener("resize", handleResize);
