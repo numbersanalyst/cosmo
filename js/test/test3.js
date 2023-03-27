3af4abbf6bae199a3a506cb236b1d9be5f07c384
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-03-27 20:07:06 +0200

    Dodanie całej serii obiektów z animacjami na poligonie 3.

--- js/test/test3.js
+++ js/test/test3.js
@@ -9,27 +9,49 @@ camera = new THREE.PerspectiveCamera(
   1,
   1000
 );
-camera.position.set(0, 0, 40);
+camera.position.set(0, 0, 250);
 renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const createTorus = (c=0xffffff, r=5,t=0.7,tSeg=180,rSeg=20,p=3,q=7) => {
-  const torusGeo = new THREE.TorusKnotGeometry(r, t, tSeg, rSeg, p, q);
-  const torusMat = new THREE.MeshPhongMaterial({ color: c, shininess: 5});
-  return new THREE.Mesh(torusGeo, torusMat);
-};
-
-const createPointLight = (color=0xffffff, i=1) => {
+const createPointLight = (color = 0xffffff, i = 1) => {
   return new THREE.PointLight(color, i);
 };
 
-const fTorus = createTorus();
-scene.add(fTorus);
-
 const l1 = createPointLight();
-l1.position.set(60,60,60);
+l1.position.set(0, 0, 40);
 scene.add(l1);
+
+const createTorus = (c = 0xffffff, r = 5, t = 0.7, tSeg = 180, rSeg = 20, p = 3, q = 7) => {
+  const torusGeo = new THREE.TorusKnotGeometry(r, t, tSeg, rSeg, p, q);
+  const torusMat = new THREE.MeshPhongMaterial({ color: c, shininess: 5 });
+  return new THREE.Mesh(torusGeo, torusMat);
+};
+
+const torusList = [];
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 8; j++) {
+    const newTorus = createTorus();
+    newTorus.position.set(20 * j - 70, 18 * i-27, 0);
+    torusList.push(newTorus);
+  }
+}
+
+torusList.forEach(torus => {
+  scene.add(torus);
+});
+
+const loop = () => {
+  torusList.forEach((torus, i) => {
+    torus.rotation.x += 0.001 * i;
+    torus.rotation.y += 0.001 * i;
+    torus.rotation.z += 0.001 * i;
+  });
+  renderer.render(scene, camera);
+  requestAnimationFrame(loop);
+}
+loop();
 
 const handleResize = () => {
   const { innerWidth, innerHeight } = window;
@@ -39,19 +61,12 @@ const handleResize = () => {
 };
 window.addEventListener("resize", handleResize);
 
-const loop = () => {
-  fTorus.rotation.y += 0.001;
-  renderer.render(scene, camera);
-  requestAnimationFrame(loop);
-}
-loop();
-
 document.body.onload = () => {
   document.querySelector("p").style.opacity = 0;
 };
 
 document.addEventListener("keydown", (event) => {
   if (event.isComposing || event.keyCode === 27) {
-      history.back();
+    history.back();
   }
 });
