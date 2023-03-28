eb3424ad207deccfbdd7d5c6fcce05e238593428
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-03-28 10:17:49 +0200

    Testowe dodanie sterowania do podstrony earth.html

--- js/earth.js
+++ js/earth.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-let scene, camera, renderer;
+let scene, camera, controls, renderer;
 
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 1000);
@@ -9,6 +9,9 @@ camera.position.set(0, 0, 50);
 renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+console.log(controls);
 
 const loop = () => {
     renderer.render(scene, camera);
