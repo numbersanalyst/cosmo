c3f45118d6dd516b924dc266d27dbabb431e5d27
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-03-30 21:07:48 +0200

    Dodano topografię i zmianę tekstury przy określonym przybliżeniu kamery w earth.js

--- js/earth.js
+++ js/earth.js
@@ -10,7 +10,7 @@ renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const createSphere = (r, wSeg, hSeg, mapUrl, bMapUrl=null) => {
+const createSphere = (r, wSeg, hSeg, mapUrl, bMapUrl = null) => {
     const sphereGeo = new THREE.SphereGeometry(r, wSeg, hSeg);
     const sphereMat = new THREE.MeshStandardMaterial({ map: new THREE.TextureLoader().load(mapUrl) });
     if (bMapUrl != null) { sphereMat.bumpMap = new THREE.TextureLoader().load(bMapUrl); }
@@ -27,12 +27,14 @@ const textures = [
     "../assets/textures/gebco_08_rev_elev_8192_custom.png"
 ];
 
-const earth = createSphere(5, 50, 50, textures[0]);
+const earthTop = createSphere(5, 50, 50, textures[0]);
+const earthUnder = createSphere(5, 50, 50, textures[1], textures[2]);
 const light = createPointLight(0xffffff, 1);
 
+earthUnder.visible = false;
 light.position.set(-50, 50, 25);
 
-scene.add(earth, camera);
+scene.add(earthTop, earthUnder, camera);
 camera.add(light);
 
 controls = new OrbitControls(camera, renderer.domElement);
@@ -41,10 +43,32 @@ controls.enablePan = false;
 controls.minDistance = 10;
 controls.maxDistance = 250;
 
+let change = false;
+
 const loop = () => {
-    if (controls.getDistance() > 30) { controls.rotateSpeed = 1; }
-    else if (controls.getDistance() > 20) { controls.rotateSpeed = 0.4; }
-    else { controls.rotateSpeed = 0.2; }
+    if (controls.getDistance() > 30) {
+        controls.rotateSpeed = 1;
+
+        if (change == true) {
+            earthTop.visible = true;
+            earthUnder.visible = false;
+
+            change = false;
+        }
+    }
+    else if (controls.getDistance() > 20) {
+        controls.rotateSpeed = 0.4;
+        
+        if (change == false) {
+            earthTop.visible = false;
+            earthUnder.visible = true;
+
+            change = true;
+        }
+    }
+    else {
+        controls.rotateSpeed = 0.2; 
+    }
     controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(loop);
