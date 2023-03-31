3e32e64df2613a47796bebc2e59c987d8114a809
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-03-31 15:28:41 +0200

    Optymalizacja i poprawa czytelności kodu w earth.js

--- js/earth.js
+++ js/earth.js
@@ -1,14 +1,26 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+const earthTexture = "../assets/textures/4k_earth_land_ocean_ice_cloud.png";
+const earthLandTexture = "../assets/textures/8k_earth_land_ocean_ice.png";
+const earthTopography = "../assets/textures/8k_earth_topography.png";
+
 let scene, camera, controls, renderer;
 
 scene = new THREE.Scene();
+
 camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(0, 0, 50);
+
 renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 10;
+controls.maxDistance = 250;
 
 const createSphere = (r, wSeg, hSeg, mapUrl, bMapUrl) => {
     const sphereGeo = new THREE.SphereGeometry(r, wSeg, hSeg);
@@ -21,35 +33,22 @@ const createPointLight = (c, i) => {
     return new THREE.PointLight(c, i);
 };
 
-const textures = [
-    "../assets/textures/4k_earth_land_ocean_ice_cloud.png",
-    "../assets/textures/8k_earth_land_ocean_ice.png",
-    "../assets/textures/8k_earth_topography.png"
-];
-
-const earthTop = createSphere(5, 50, 50, textures[0]);
-const earthUnder = createSphere(5, 50, 50, textures[1], textures[2]);
+const earthTop = createSphere(5, 50, 50, earthTexture);
+const earthUnder = createSphere(5, 50, 50, earthLandTexture, earthTopography);
 const light = createPointLight(0xffffff, 1);
 
 earthUnder.visible = false;
 light.position.set(-50, 50, 25);
 
-scene.add(earthTop, earthUnder, camera);
 camera.add(light);
-
-controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.minDistance = 10;
-controls.maxDistance = 250;
+scene.add(earthTop, earthUnder, camera);
 
 let change = false;
 
 const loop = () => {
     if (controls.getDistance() > 30) {
-        controls.rotateSpeed = 1;
-
         if (change == true) {
+            controls.rotateSpeed = 1;
             earthTop.visible = true;
             earthUnder.visible = false;
 
@@ -57,9 +56,8 @@ const loop = () => {
         }
     }
     else if (controls.getDistance() > 20) {
-        controls.rotateSpeed = 0.4;
-
         if (change == false) {
+            controls.rotateSpeed = 0.4;
             earthTop.visible = false;
             earthUnder.visible = true;
 
@@ -67,7 +65,11 @@ const loop = () => {
         }
     }
     else {
-        controls.rotateSpeed = 0.2;
+        if (change == true) {
+            controls.rotateSpeed = 0.2;
+
+            change = false;
+        }
     }
     controls.update();
     renderer.render(scene, camera);
