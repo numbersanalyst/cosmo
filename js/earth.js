34d650de8a3aec81669fdd3f6fcc5836054e6048
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2023-04-17 14:30:23 +0200

    Fix paths to textures in earth.js

--- js/earth.js
+++ js/earth.js
@@ -6,9 +6,9 @@ const textures = {
   earth: textureLoader.load(
     '/public/4k_earth_land_ocean_ice_cloud.png'
   ),
-  earthLand: textureLoader.load('/public/textures/8k_earth_land_ocean_ice.png'),
-  earthTopo: textureLoader.load('/public/textures/8k_earth_topography.png'),
-  stars: textureLoader.load('/public/textures/8k_stars_milky_way.jpg'),
+  earthLand: textureLoader.load('/public/8k_earth_land_ocean_ice.png'),
+  earthTopo: textureLoader.load('/public/8k_earth_topography.png'),
+  stars: textureLoader.load('/public/8k_stars_milky_way.jpg'),
 };
 const colorLight = new THREE.Color('hsl(255, 100%, 100%)');
 
