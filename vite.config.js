cea9dfe3e7dab81c9c054f69ef8fa3edbbf5cd2d
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-04-17 20:44:31 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-        select: resolve(root, 'subpages/select.html'),
-        earth: resolve(root, 'subpages/earth.html'),
+        select: resolve(root, 'select/index.html'),
+        earth: resolve(root, 'earth/index.html'),
+        test1: resolve(root, 'test/1/index.html'),
+        test2: resolve(root, 'test/2/index.html'),
+        test3: resolve(root, 'test/3/index.html'),
       },
     },
   },
