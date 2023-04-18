716f91ecc889c81fba68d7e28b0f7d81695e8753
numbersanalyst <124043202+numbersanalyst@users.noreply.github.com>
2023-04-18 09:56:50 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,13 @@ export default defineConfig({
       input: {
         main: resolve(root, 'index.html'),
         select: resolve(root, 'select/index.html'),
+        mercury: resolve(root, 'mercury/index.html'),
+        venusAtmosphere: resolve(root, 'venus-atmosphere/index.html'),
+        venusSurface: resolve(root, 'venus-surface/index.html'),
         earth: resolve(root, 'earth/index.html'),
+        mars: resolve(root, 'mars/index.html'),
+        jupiter: resolve(root, 'jupiter/index.html'),
+        saturn: resolve(root, 'saturn/index.html'),
         test1: resolve(root, 'test/1/index.html'),
         test2: resolve(root, 'test/2/index.html'),
         test3: resolve(root, 'test/3/index.html'),
