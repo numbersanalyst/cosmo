40175329b8187f387d256f8aceff5b0520909e29
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-04-24 19:33:47 +0200

    Add new function

---
+++ src/common/createSphereObj.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+
+const createSphere = (r, wSeg, hSeg, mapUrl, bMapUrl, backSide) => {
+  const sphereGeo = new THREE.SphereGeometry(r, wSeg, hSeg);
+  const sphereMat = new THREE.MeshStandardMaterial({ map: mapUrl });
+  if (bMapUrl) {
+    sphereMat.bumpMap = bMapUrl;
+    sphereMat.bumpScale = 0.1;
+  }
+  if (backSide) {
+    sphereMat.side = THREE.BackSide;
+    sphereMat.transparent = true;
+    sphereMat.opacity = 0.8;
+  }
+  const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+  const obj = new THREE.Object3D();
+  obj.add(mesh);
+  return { mesh, obj };
+};
+
+export default createSphere;
