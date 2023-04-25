0cf7cf9dfde6a8a7d093f906ea846ae93d2b3be6
numbersanalyst <numbersanalyst@users.noreply.github.com>
2023-04-25 16:06:42 +0000

    deploy: b518ff46d52d09cb122cbe0373daefb293798b13

--- assets/createRing-ffbe4998.js
+++
@@ -1,1 +0,0 @@
-import{R as i,M as o,D as c,a as d}from"./three.module-5fde15d4.js";const M=(e,a,t,r)=>{const n=new i(e,a,t),s=new o({map:r,side:c,transparent:!0,depthWrite:!1});return new d(n,s)};export{M as c};
