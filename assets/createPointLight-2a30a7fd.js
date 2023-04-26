baed552c9ab1f91751cd6a646f34a9fa4e2234e5
numbersanalyst <numbersanalyst@users.noreply.github.com>
2023-04-26 19:09:23 +0000

    deploy: 550bfb0693a44fcfa79bb9462bb9cd46e291d2de

--- assets/createPointLight-2a30a7fd.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./three.module-c996bb13.js";const n=(t,o,r)=>new e(t,o,r);export{n as c};
---
+++ assets/createPointLight-a525ace3.js
@@ -0,0 +1,1 @@
+import{f as e}from"./three.module-0fa09e44.js";const n=(t,o,r)=>new e(t,o,r);export{n as c};
