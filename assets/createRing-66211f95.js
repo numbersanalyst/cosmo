d24d4561fc1f2a84014ec488464a9d013dc84bc0
numbersanalyst <numbersanalyst@users.noreply.github.com>
2023-04-26 15:58:59 +0000

    deploy: d14a5a69f891f38ced628f26d9a0b164f4b2106b

--- assets/createRing-66211f95.js
+++
@@ -1,1 +0,0 @@
-import{R as i,M as o,D as c,a as d}from"./three.module-a0a9b859.js";const M=(e,a,t,r)=>{const n=new i(e,a,t),s=new o({map:r,side:c,transparent:!0,depthWrite:!1});return new d(n,s)};export{M as c};
--- assets/vertex-1c6f4e85.js
+++
@@ -1,1 +0,0 @@
-const i="void main(){gl_Position=projectionMatrix*modelViewMatrix*vec4(position.x,position.y,position.z,1.0);}";export{i as v};
