585d7aed6324d46da8360867ed0ecbd8752e60b7
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-04-26 17:58:02 +0200

    Simplify the code

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,3 +1,3 @@
 void main() {
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, position.z, 1.0);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
